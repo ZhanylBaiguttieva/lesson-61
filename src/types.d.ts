1c665e2d79eb5ef9ff5357b0316a714225692d96
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-22 23:52:48 +0600

    Added types APICountry and ApiFullCountry

---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+export interface APIList {
+  name: string;
+}
+
+export interface APICountry {
+  alpha3Code: string;
+  name: string;
+}
+
+export interface ApiFullCountry {
+  alpha3Code: string;
+  name: string;
+  capital: string;
+  subregion: string;
+  population: number;
+}
